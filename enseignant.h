549e876f0bdb933131e376019b8911e6320d8b9e
Mighritch <100856042+Mighritch@users.noreply.github.com>
2024-07-04 15:38:38 +0200

    Create enseignant.h

---
+++ enseignant.h
@@ -0,0 +1,39 @@
+#ifndef ENSEIGNANT_H
+#define ENSEIGNANT_H
+
+#include <QString>
+#include <QSqlQueryModel>
+
+class enseignant
+{
+public:
+    enseignant();
+    enseignant(const QString &nom, const QString &prenom, int cin, QString &matiere);
+
+       // Accesseurs
+       QString getNom() const;
+       void setNom(const QString &nom);
+
+       QString getPrenom() const;
+       void setPrenom(const QString &prenom);
+
+       int getCin() const;
+       void setCin(int cin);
+
+       QString getMatiere() const;
+       void setMatiere(const QString &matiere);
+
+           bool ajout();
+           bool existeCin(int cin);
+           QSqlQueryModel *affiche();
+           QSqlQueryModel *recherche(const QString &nom);
+           QSqlQueryModel *cherche(const QString &matiere);
+           bool supprime(int cin);
+           bool modifie();
+
+private:
+QString nom,prenom,matiere;
+int cin;
+};
+
+#endif // ENSEIGNANT_H
